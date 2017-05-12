32aa7674c3b814a9bc822bf8da00ce02dbea5107
Nicolas Jakob <nicolas.jakob@teemo.de>
2017-05-12 19:12:28 +0200

    Fix Algorithm

--- exercise_sheet2_2.py
+++ exercise_sheet2_2.py
@@ -5,18 +5,18 @@ import math
 
 
 np.set_printoptions(precision=3)
-sign = functools.partial(math.copysign, 1)
 
-x = np.array([[2, 4],
-              [1, 0.5],
-              [0.5, 1.5],
-              [0, 0.5]])
+
+x = np.array([[1.0, 2, 4],
+              [1.0,1, 0.5],
+              [1.0,0.5, 1.5],
+              [1.0,0, 0.5]])
 
 w = np.array([0.0, 1.0, -1.0])
 
 trouble = True
 
-inc = 0.1
+inc = 0.25
 
 yh = 0
 y = 0
@@ -26,20 +26,20 @@ steps = 0
 while trouble:
     trouble = False
     for i in range(0, 4):
-        steps += 1
         if i < 2:
             y = 1
         else:
             y = -1
-        yhtemp = 1 * w[0] + x[i][0] * w[1] + x[i][1] * w[2]
-        yh = sign(yhtemp)
-        if y - yh != 0:
+        yhtemp = x[i][0] * w[0] + x[i][1] * w[1] + x[i][2] * w[2]
+        yh = np.sign(yhtemp)
+        if y -yh != 0:
+            steps += 1
             trouble = True
             print("Problem with: w:" + str(w))
             print("x: " + str(x[i]))
             for j in range(0, 3):
                 w_old = w[j]
-                w[j] = w[j] + inc * (y - yh) * x[i][j - 1]
+                w[j] = w[j] + inc * y * x[i][j]
                 print("Fitting: old w" + str(j) + " : " + str(w_old) + " to w: " + str(w) + " for x: " + str(x[i]))
 
         else:
