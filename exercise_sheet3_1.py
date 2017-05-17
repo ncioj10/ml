33e80d0a40b67859e3bb5393e1e2193124d7dd56
Nicolas Jakob <nicolas.jakob@teemo.de>
2017-05-17 20:29:00 +0200

    Add Sheet3_1

---
+++ exercise_sheet3_1.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+xm = np.matrix([[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]]).astype(float)
+ym = np.matrix([150,155,150,170,160,175]).astype(float).T
+wm = np.matrix([0,0])
+
+running = True
+inc = 0.0001
+
+
+
+m, c = np.linalg.lstsq(xm, ym)[0]
+print (m, c)
+counter = 0
+
+while running:
+    weights = np.matrix([0, 0])
+    counter +=1
+    running = False
+    for x,y in zip(xm,ym):
+        yh = x * wm.T
+        diff = (y - yh)
+        weights = weights + diff[0][0] * x
+
+        if diff[0][0] != 0.0  :
+            running = True
+
+    wm = wm + weights * inc
+    if counter > 10000:
+        break
+
+
+
+print(wm)
+
+
+
+
+
+
